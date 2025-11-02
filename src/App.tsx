503d53ef9e7b557d5bf94053fb72fd345ab62204
vladislavzilin-byte <vladislavzilin@gmail.com>
2025-11-02 23:23:27 +0000

    Add routing for Success and Cancel pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-
 import Home from './pages/Home'
 import Shop from './pages/Shop'
 import Login from './pages/Login'
@@ -19,6 +18,19 @@ import Portfolio from './pages/Portfolio'
 import Shop from './pages/Shop'
 import Training from './pages/Training'
 import Contacts from './pages/Contacts'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/success" element={<Success />} />
+      <Route path="/cancel" element={<Cancel />} />
+    </Routes>
+  )
+}
 
 type Lang = 'lt' | 'en' | 'ru'
 const palette = { bg:'#000000' }
